91af0a35214dbf50d9389ad90bfe42d9104840ec
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-04-25 14:12:03 +0200

    RPG Party Game added

---
+++ RPG Game project/RPG-3/Jatek.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace RPG_3
+{
+    class Jatek
+    {
+
+        List<Karakter> parti = new List<Karakter>();
+        List<Ellenfel> ellenfelek = new List<Ellenfel>();
+
+        public void Beolvas(string partiFileName, string ellenfelekFileName)
+        {
+            parti.Clear();
+            ellenfelek.Clear();
+            
+            TextFileReader reader = new(partiFileName);
+            char[] separators = { ' ', '\t' };
+            string[] tokens;
+            while (reader.ReadLine(out string line))
+            {
+                tokens = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                Karakter ujKarakter = null;
+                switch(tokens[1])
+                {
+                    case "harcos":
+                        ujKarakter = new Harcos(tokens[0], int.Parse(tokens[2]), int.Parse(tokens[3]), int.Parse(tokens[4]));
+                        break;
+                    case "kósza":
+                        ujKarakter = new Kosza(tokens[0], int.Parse(tokens[2]), int.Parse(tokens[3]), int.Parse(tokens[4]));
+                        break;
+                    case "varázsló":
+                        ujKarakter = new Varazslo(tokens[0], int.Parse(tokens[2]), int.Parse(tokens[3]), int.Parse(tokens[4]));
+                        break;
+                    default: break;
+                }
+
+                parti.Add(ujKarakter);
+            }
+
+            reader = new(ellenfelekFileName);
+            while (reader.ReadLine(out string line))
+            {
+                tokens = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                // biztos ork
+                ellenfelek.Add(new Ork(int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3])));
+            }
+
+        }
+
+        public (string, List<string>) Lejatszik()
+        {
+            while(!(parti.Count() == 0 || ellenfelek.Count() == 0))
+            {
+                kor();
+            }
+
+            if(parti.Count() != 0)
+            {
+                // karakterek visszadása
+
+                List<string> players = new List<string>();
+
+                foreach(Karakter k in parti)
+                {
+
+                    players.Add(k.getName());
+                }
+
+                return ("nyert", players);
+                
+
+            } else
+            {
+                List<string> emptyList = new List<string>();
+
+                return ("vesztett", emptyList);
+            }
+
+        }
+
+
+        public void kor()
+        {
+            foreach(Karakter karakter in parti)
+            {
+                karakter.tamad(parti, ellenfelek);
+
+            }
+
+            HPSzuresE();
+
+            if(ellenfelek.Count() == 0)
+            {
+                return;
+            } else
+            {
+                ellenfelek[0].tamad(parti);
+            }
+
+            HPSzuresP();
+
+        }
+
+        public void HPSzuresE()
+        {
+            List<Ellenfel> ujEllenfelek = new List<Ellenfel>();
+
+            foreach(Ellenfel ellenfel in ellenfelek)
+            {
+                if(ellenfel.eletbenVan())
+                {
+                    ujEllenfelek.Add(ellenfel);
+                }
+            }
+
+            ellenfelek = ujEllenfelek;
+        }
+        public void HPSzuresP()
+        {
+            List<Karakter> ujParti = new List<Karakter>();
+
+            foreach (Karakter karakter in parti)
+            {
+                if (karakter.eletbenVan())
+                {
+                    ujParti.Add(karakter);
+                }
+            }
+
+            parti = ujParti;
+
+        }
+    } 
+}
---
+++ RPG Game project/RPG-3/Szereplo.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// zöld kör public
+// teljes rombusz private
+// üres rombusz protected
+
+namespace RPG_3
+{
+    public abstract class Szereplo
+    {
+        protected int HP;
+        protected int vedelem;
+        protected int sebzes;
+
+
+        public Szereplo(int HP, int vedelem, int sebzes)
+        {
+            this.HP = HP;
+            this.vedelem = vedelem;
+            this.sebzes = sebzes;
+        }
+
+        public bool eletbenVan()
+        {
+            return HP >= 0;
+        }
+
+        public void sebzodik(Szereplo tamado)
+        {
+            int tenylegesSebzes = tamado.sebzes - vedelem;
+
+            if(tenylegesSebzes > 0)
+            {
+                HP = HP - tenylegesSebzes;
+            }
+        }
+
+
+    }
+
+
+    public abstract class Ellenfel : Szereplo
+    {
+        public Ellenfel(int HP, int vedelem, int sebzes) : base(HP, vedelem, sebzes) { }
+
+        public abstract void tamad(List<Karakter> parti);
+    }
+
+    public abstract class Karakter : Szereplo
+    {
+        protected string nev;
+
+        public Karakter(string nev, int HP, int vedelem, int sebzes) : base(HP, vedelem, sebzes)
+        {
+            this.nev = nev;   
+        }
+
+
+        public abstract void tamad(List<Karakter> parti, List<Ellenfel> ellenfelek);
+
+        public string getName()
+        {
+            return nev;
+        }
+
+    }
+
+
+    public class Harcos : Karakter
+    {
+        public Harcos(string nev, int HP, int vedelem, int sebzes) : base(nev, HP, vedelem, sebzes) { }
+
+        public override void tamad(List<Karakter> parti, List<Ellenfel> ellenfelek)
+        {
+            int sajatIndex = 0;
+
+            for (int i = 0; i < parti.Count(); i++)
+            {
+                if (parti[i] == this)
+                {
+                    sajatIndex = i;
+                }
+            }
+
+            if (sajatIndex != 0)
+            {
+                return;
+            }
+            else
+            {
+                ellenfelek[0].sebzodik(this);
+            }
+
+        }
+    }
+
+    public class Kosza : Karakter
+    {
+        public Kosza(string nev, int HP, int vedelem, int sebzes) : base(nev, HP, vedelem, sebzes) { }
+
+        public override void tamad(List<Karakter> parti, List<Ellenfel> ellenfelek)
+        {
+            ellenfelek[0].sebzodik(this);
+        }
+    }
+
+    public class Varazslo : Karakter
+    {
+        public Varazslo(string nev, int HP, int vedelem, int sebzes) : base(nev, HP, vedelem, sebzes) { }
+
+
+        public override void tamad(List<Karakter> parti, List<Ellenfel> ellenfelek)
+        {
+            ellenfelek[0].sebzodik(this);
+
+            if (ellenfelek.Count() > 1)
+            {
+                ellenfelek[1].sebzodik(this);
+            }
+        }
+    }
+
+    public class Ork : Ellenfel
+    {
+
+        public Ork(int HP, int vedelem, int sebzes) : base(HP, vedelem, sebzes) { }
+        
+        public override void tamad(List<Karakter> parti)
+        {
+            parti[0].sebzodik(this);
+        }
+    }
+}
